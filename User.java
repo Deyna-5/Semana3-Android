634b6e49103b75de27aad07d80d9054002b99b95
Deyna-5 <alexahoshiop@gmail.com>
2024-04-09 14:36:46 -0400

    Add: comments

--- User.java
+++ User.java
@@ -1,23 +1,28 @@
 public class User {
+    // Agregamos los atributos requeritos del usuario.
     private String Name;
     private Integer Age;
 
+    // Creamos el método para inicializar el usuario con datos por defecto.
     public void user(){
         this.Name = "";
         this.Age = 0;
     }
 
+    // Creamos el método setter para el nombre de usuario
     public void setName(String name){
         this.Name = name;
     }
 
+    // Creamos el método setter para la edad del usuario
     public void setAge(Integer age){
         this.Age = age;
     }
 
+    // Creamos un método para pintar la data del usuario.
     public void showUserData() {
         System.out.print("\n-------------------------------------------------------------------------\n");
-        System.out.print("El usuario se llama:  " + Name + "\nTiene " + Age + " anos");
+        System.out.print("El usuario se llama:  " + Name + "\nTiene " + Age + " años");
         System.out.print("\n-------------------------------------------------------------------------\n");
     }
 }
--- Vehicle.java
+++ Vehicle.java
@@ -9,6 +9,7 @@ public class Vehicle {
     private Integer Capacity;
     private User user;
 
+    // Creamos el método para inicializar nuestro vehiculo con data por defecto
     public Vehicle(){
         this.Brand = "";
         this.Model = "";
@@ -18,56 +19,69 @@ public class Vehicle {
         this.user = new User();
     }
 
+    // Creamos el getter para la marca
     public String getBrand() {
         return Brand;
     }
-
+    
+    // Creamos el setter para la marca
     public void setBrand(String brand) {
         this.Brand = brand;
     }
 
+    //  Creamos el getter para modelo
     public String getModel() {
         return Model;
     }
 
+    // Creamos el setter para modelo
     public void setModel(String model) {
         this.Model = model;
     }
 
+    // Creamos el getter para cilindrada
     public Integer getCilindrada() {
         return Cilindrada;
     }
 
+    // Creamos el setter para cilindrada
     public void setCilindrada(Integer cilindrada) {
         this.Cilindrada = cilindrada;
     }
 
+    //  Creamos el getter para tipo de combustible
     public String getTypeOfFuel() {
         return TypeOfFuel;
     }
 
+    // Creamos el setter para el tipo de combustible
     public void setTypeOfFuel(String typeOfFuel) {
         this.TypeOfFuel = typeOfFuel;
     }
 
+    // Creamos el getter para la capacidad
     public Integer getCapacity() {
         return Capacity;
     }
 
+    // Creamos el setter para la capacidad
     public void setCapacity(Integer capacity) {
         this.Capacity = capacity;
     }
 
+    // Creamos el setter para el usuario
     public void setUser(User user) {
         this.user = user;
     }
 
+    //  Creamos un método para imprimir la información del vehiculo
     public void showVehicleData() {
         System.out.print("\n-------------------------------------------------------------------------\n");
         System.out.print("El vehiculo cuenta con las siguientes caracteristicas:" + "\nEs de la marca: " + Brand + "\nEs el modelo: " + Model + "\nTiene una cilindrada de: " + Cilindrada + "\nTiene un combustible de tipo: " + TypeOfFuel + "\ny por ultimo cuenta con una capacidad para " + Capacity + " personas");
         System.out.print("\n-------------------------------------------------------------------------\n");
     }
 
+    // Creamos el main donde se llevará acabo toda la ejecución
     static void Main() {
         User user = new User();
         Scanner scanner = new Scanner(System.in);
